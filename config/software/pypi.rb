adbcf1c2ed73633200f82aa6b01d251ad50ee957
Masashi Terui <masashi.terui@jig-saw.com>
2016-03-16 18:52:32 +0900

    fix old url

--- config/software/pypi.rb
+++ config/software/pypi.rb
@@ -1,8 +1,6 @@
 name "pypi"
 
-dependency "curl"
-
 build do
-  command "curl -kL https://raw.github.com/pypa/pip/master/contrib/get-pip.py | #{install_dir}/embedded/bin/python"
+  command "curl -kL https://bootstrap.pypa.io/get-pip.py | #{install_dir}/embedded/bin/python"
   command "ln -fs #{install_dir}/embedded/bin/pip #{install_dir}/bin/pip"
 end
--- omnibus.rb
+++ omnibus.rb
@@ -17,12 +17,12 @@
 #
 # Alternatively you can tune the individual values
 # ------------------------------------------------
-# cache_dir     './local/omnibus/cache'
-# git_cache_dir './local/omnibus/cache/git_cache'
-# source_dir    './local/omnibus/src'
-# build_dir     './local/omnibus/build'
-# package_dir   './local/omnibus/pkg'
-# package_tmp   './local/omnibus/pkg-tmp'
+cache_dir     './local/omnibus/cache'
+git_cache_dir './local/omnibus/cache/git_cache'
+source_dir    './local/omnibus/src'
+build_dir     './local/omnibus/build'
+package_dir   './local/omnibus/pkg'
+package_tmp   './local/omnibus/pkg-tmp'
 
 # Disable git caching
 # ------------------------------
